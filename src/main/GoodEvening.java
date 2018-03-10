12902d96f1c8a45f09b5273a9c69b198d2c8989d
aseladamisaeva <aseladamisaeva@10.1.10.173>
2018-03-10 14:23:17 -0600

    you are my brother

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -3,6 +3,8 @@ package main;
 public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println(" good evening");
+		System.out.println("uli come come");
+		System.out.println("uli you are taw");
 	}
 
 }
